520cc89e6c1e92020b145e68590c31328e7a8e91
nayumtamboli <nayumtamboli@gmail.com>
2026-02-05 23:11:41 +0530

    Add search value display for selected fruits
    Display search value for selected fruits in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ st.write("The name on your Smoothie will be ", name_on_order)
 cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
@@ -36,6 +35,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
